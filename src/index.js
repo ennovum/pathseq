c81af48a97f6edea2ccdd3ef7263ba69fbd71df7
Grzegorz Wasiak <g.wasiak@gmail.com>
2017-03-03 23:44:02 +0100

    library export as module fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import pathseq from './pathseq/pathseq';
 
+export default pathseq;
+export {pathseq};
+
 // Webpack can't export default ESM properly (planned for 2.4 release)
-module.exports = pathseq;
+try {
+    module.exports = pathseq;
+} catch (err) {
+    err; // noop
+}
